9b631db9872f553941b6260bbbe4b621ca193220
Mattia Di Paolo <mattiadipaolo179@gmail.com>
2024-08-16 15:45:28 +0200

    Finito il Select Team ed aggiustato il game menu con l'aggiornamento di bandiere e nomi dei giocatori

--- Head Football/Assets/Scripts/UITeam.cs
+++ Head Football/Assets/Scripts/UITeam.cs
@@ -1,12 +1,15 @@
-/*using System.Collections;
+/*
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UITeam : MonoBehaviour {
+    public Sprite[] flagTeam;
+
     public static UITeam instance;
 
-    public Image selectedFlag; 
+    public Image selectedFlagImage; 
     public Text selectedTeamName;
     
     private void Awake() {
@@ -18,9 +21,13 @@ public class UITeam : MonoBehaviour {
         }
     }
 
-    // Metodo chiamato al click su una bandiera
+     Metodo chiamato al click su una bandiera
     public void OnFlagClicked(Sprite flag, string teamName) {
         selectedFlagImage.sprite = flag; // Imposta l'immagine della bandiera selezionata
         selectedTeamName.text = teamName; // Imposta il nome del team selezionato
     }
-}*/
+    
+}
+*/
+
+
