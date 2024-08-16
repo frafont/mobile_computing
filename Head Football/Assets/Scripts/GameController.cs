f8059154bff943d1322b49d2f600f58bed330811
Mattia Di Paolo <mattiadipaolo179@gmail.com>
2024-08-16 15:45:28 +0200

    Finito il Select Team ed aggiustato il game menu con l'aggiornamento di bandiere e nomi dei giocatori

--- Head Football/Assets/Scripts/GameController.cs
+++ Head Football/Assets/Scripts/GameController.cs
@@ -2,28 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class GameController : MonoBehaviour
 {
+    //private GameObject Player,AI;
+    public GameObject[] playerPrefabs;
+    
+    public GameObject[] EnemyPrefabs;
+    public GameObject[] NationsName;
+    public GameObject Nameplayer;
+    public GameObject NameEnemy;
+
     public static GameController instance;
+    public Sprite[] Flags;
+    public GameObject PlayerFlag;
+    public GameObject EnemyFlag;
 
     public Text txt_GoalPlayer, txt_GoalEnemy;
+     public static int selectedPlayerIndex = -1;
+     public static int selectedEnemyIndex=-1;
+
 
     public  int scorePlayer, scoreEnemy;
-    public bool isScore, EndMatch;
 
+    public bool isScore, EndMatch;
+    
     private void Awake()
     {
         if(instance == null)
         {
             instance = this;
         }
+
+      if (selectedPlayerIndex != -1 && selectedEnemyIndex!=-1 ) 
+        {
+            Instantiate(playerPrefabs[selectedPlayerIndex], new Vector3(0, 0, 0), Quaternion.identity);
+
+            PlayerFlag.GetComponent<Image>().sprite =Flags[selectedPlayerIndex];
+            Nameplayer.GetComponent<Text>().text=NationsName[selectedPlayerIndex].GetComponent<Text>().text;
+
+            Instantiate(EnemyPrefabs[selectedEnemyIndex], new Vector3(1.0f, 0, 0), Quaternion.identity);
+
+            EnemyFlag.GetComponent<Image>().sprite=Flags[selectedEnemyIndex];
+            NameEnemy.GetComponent<Text>().text=NationsName[selectedEnemyIndex].GetComponent<Text>().text;
+            NameEnemy=NationsName[selectedEnemyIndex];
+        }
+            
     }
-    // Start is called before the first frame update
-    void Start()
-    {
         
-    }
+
 
     // Update is called once per frame
     void Update()
@@ -31,4 +59,5 @@ public class GameController : MonoBehaviour
         txt_GoalPlayer.text = scorePlayer.ToString();
         txt_GoalEnemy.text = scoreEnemy.ToString();
     }
+
 }
---
+++ Head Football/Assets/Scripts/Select team.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SelectTeam : MonoBehaviour
+{
+    public Button[] nationButtons;  // Buttons for each team
+    public GameObject[] Playertarget;
+    public GameObject[] Enemytarget;
+    // Start is called before the first frame update
+    private int IndicePrec=-1;
+    void Start()
+    {
+        
+         for (int i = 0; i < nationButtons.Length; i++)
+        {
+
+            if(GameController.selectedPlayerIndex==-1){
+                 int index = i;  // Local copy to avoid closure problem in lambda
+            nationButtons[i].onClick.AddListener(() => HandleFlagClick(index));
+            
+            }
+            
+        }
+       
+    }
+    public enum SelectionMode{
+         SelectingPlayer,
+         SelectingOpponent
+    }
+
+    public SelectionMode currentSelectionMode = SelectionMode.SelectingPlayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     public void SelectPlayer(int playerIndex)
+    {
+      
+        if (playerIndex >= 0 && playerIndex < nationButtons.Length )
+        {
+               Playertarget[playerIndex].SetActive(true);
+       
+            GameController.selectedPlayerIndex=playerIndex;
+      
+           
+
+            // Instantiate the selected player prefab at a specific location or set it active
+           // GameObject player = Instantiate(playerPrefabs[playerIndex], new Vector3(0, 0, 0), Quaternion.identity);
+            
+
+            // Here, add any logic that should occur right after selecting a player, such as updating UI
+           
+           // Debug.Log("Selected player from nation: " + player.name);
+        }
+        else
+        {
+            Debug.LogError("Invalid player index selected.");
+        }
+    }
+  
+
+    public void ButtonBack()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+     public void ButtonPlay()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    
+    void HandleFlagClick(int index)
+{
+    switch (currentSelectionMode)
+    {
+        case SelectionMode.SelectingPlayer:
+            SelectPlayer(index);
+            currentSelectionMode = SelectionMode.SelectingOpponent; // Passa alla selezione dell'avversario
+            break;
+        case SelectionMode.SelectingOpponent:
+        if(IndicePrec!=-1){
+            Enemytarget[IndicePrec].SetActive(false);
+        }
+            SelectOpponent(index);
+            // Gestisci il passaggio alla fase successiva del gioco o altro
+
+            break;
+
+}
+}
+
+public void SelectOpponent(int index)
+{
+   
+
+    if (index >= 0 && index < nationButtons.Length && index != GameController.selectedPlayerIndex)
+    {
+           Enemytarget[index].SetActive(true);
+           IndicePrec= index;
+        GameController.selectedEnemyIndex = index;
+
+        Debug.Log("Opponent selected: " + nationButtons[index].name);
+        // Potresti voler procedere al caricamento della scena del gioco o a un'altra preparazione
+    }
+    else
+    {
+        Debug.LogError("Invalid selection for opponent.");
+    }
+}
+
+
+
+}
